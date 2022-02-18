4cc9d9f1179ec77ef902e80fdb25dd2f815af918
Jesus Rodriguez <jesuse1661@gmail.com>
2022-02-18 00:14:20 -0700

    Esta es la primer version incluye el prendido y apagado de un foco

---
+++ src/components/Foco.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import {Image,TouchableOpacity,View} from 'react-native';
+const Foco = () => {
+  const [estatus, setEstatus] = useState(false);
+  return (
+  <View>
+      <TouchableOpacity onPress={ ()=> { setEstatus(!estatus) } }>
+      <Image source={ estatus ? require('../../img/focoOn.png') : require('../../img/focoOff.png') }
+        style={{width:300, height:300}}
+      />
+      </TouchableOpacity>
+  </View>
+  )
+};
+
+export default Foco;
